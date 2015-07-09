d20ce45d8208060533f6d463631220ec763211aa
Benjamin Friedrich <benjamin.friedrich@zalando.de>
2015-07-09 14:08:11 +0200

    lazy retrieval of configuration parameters

--- corleone/app/security/OAuth2Constants.scala
+++ corleone/app/security/OAuth2Constants.scala
@@ -3,17 +3,17 @@ package security
 import play.api.Play
 
 object OAuth2Constants {
-  private val configuration = Play.current.configuration
+  private lazy val configuration = Play.current.configuration
 
-  val accessTokenUrl = configuration.getString("oauth2.access.token.url").get
-  val tokenInfoUrl = configuration.getString("oauth2.token.info.url").get
-  val callbackUrl = configuration.getString("oauth2.callback.url").get
-  val authorizationUrl = configuration.getString("oauth2.authorization.url").get
-  val requestTimeout =configuration.getLong("oauth2.request.timeout").get
-  val expiryTimeLimitForTokenRefreshInSeconds = configuration.getInt("oauth2.token.refresh.expiry.limit").get
-  val isOAuth2Enabled = configuration.getBoolean("oauth2.enabled").get
-  val credentialsFilePath = configuration.getString("oauth2.credentials.filePath").get
-  val credentialsCacheExpiryTime = configuration.getLong("oauth2.credentials.cache.expiry.time").get
+  lazy val accessTokenUrl = configuration.getString("oauth2.access.token.url").get
+  lazy val tokenInfoUrl = configuration.getString("oauth2.token.info.url").get
+  lazy val callbackUrl = configuration.getString("oauth2.callback.url").get
+  lazy val authorizationUrl = configuration.getString("oauth2.authorization.url").get
+  lazy val requestTimeout =configuration.getLong("oauth2.request.timeout").get
+  lazy val expiryTimeLimitForTokenRefreshInSeconds = configuration.getInt("oauth2.token.refresh.expiry.limit").get
+  lazy val isOAuth2Enabled = configuration.getBoolean("oauth2.enabled").get
+  lazy val credentialsFilePath = configuration.getString("oauth2.credentials.filePath").get
+  lazy val credentialsCacheExpiryTime = configuration.getLong("oauth2.credentials.cache.expiry.time").get
 
   val SESSION_KEY_ACCESS_TOKEN = "oauth2_access_token"
   val SESSION_KEY_REFRESH_TOKEN = "oauth2_refresh_token"
